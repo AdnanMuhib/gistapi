75072470414f6d032c5b2e8c57cb0db9decc8244
Adnan Muhib <adnan.muhib@rocketmail.com>
2021-02-09 05:45:21 +0500

    Integrate GistAPI in GistList Component and display Gists

--- src/components/GistList.js
+++ src/components/GistList.js
@@ -1,4 +1,38 @@
-import React, { useEffect, useState } from 'react'
-const GistList = () => <></>
+import React, { useEffect, useState } from "react";
+import { getPublicGists } from "../services/gistService";
+import Gist from "./Gist";
+import styled from "styled-components";
 
-export default GistList
+const GistList = () => {
+  const [gists, setGists] = useState([]);
+
+  useEffect(() => {
+    getPublicGists()
+      .then((response) => {
+        setGists(response.data);
+        console.log(response.data);
+      })
+      .catch((err) => {
+        console.error(err.message);
+      });
+  }, []);
+
+  return (
+    <GistListWrapper>
+      {gists.map((gist) => {
+        return <Gist key={gist.id} gist={gist} />;
+      })}
+    </GistListWrapper>
+  );
+};
+
+const GistListWrapper = styled.div`
+  width: 60%;
+  margin-left: auto;
+  margin-right: auto;
+  @media (max-width: 768px) {
+    width: 100%;
+    padding: 20px;
+  }
+`;
+export default GistList;
