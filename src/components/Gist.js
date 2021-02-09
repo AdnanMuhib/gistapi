b0d89dc152c78cd2ac4720033c8aba5abdbff37a
Adnan Muhib <adnan.muhib@rocketmail.com>
2021-02-10 01:35:36 +0500

    Show Gist Files, Gist Description and Link them

--- src/components/Gist.js
+++ src/components/Gist.js
@@ -1,19 +1,24 @@
 import React from "react";
-
-import GistHeader from "./GistHeader";
-
+import styled from "styled-components";
+import Octicon from "react-octicon";
+import GistHeader, { Link } from "./GistHeader";
 const Gist = ({ gist }) => {
   const { owner: gist_owner } = gist;
   const {
+    description = "",
     created_at,
     updated_at,
     files = {},
     comments = 0,
     forks_url,
     comments_url,
+    html_url = "#",
   } = gist;
+
+  const fileNames = Object.keys(files);
+
   return (
-    <div className="gist-wrapper">
+    <>
       <GistHeader
         gist_owner={gist_owner}
         gist_meta={{
@@ -23,11 +28,43 @@ const Gist = ({ gist }) => {
           comments,
           comments_url,
           forks_url,
+          html_url,
         }}
       />
-      <hr />
-    </div>
+      <GistDescription>{description}</GistDescription>
+      <GistFiles>
+        {fileNames.map((f) => {
+          return (
+            <Link href={files[f].raw_url} target="new" key={f}>
+              <Octicon name="file">{f}</Octicon>
+            </Link>
+          );
+        })}
+      </GistFiles>
+      <Separator />
+    </>
   );
 };
 
 export default Gist;
+
+const GistDescription = styled.p`
+  padding-left: 15px;
+  width: 100%;
+  font-size: 17px;
+`;
+
+const GistFiles = styled.div`
+  display: inline-block;
+  color: #0366d6;
+  width: 100%;
+  margin-left: 20px;
+  margin-bottom: 20px;
+  & > a {
+    margin-right: 15px;
+    padding: 5px;
+  }
+`;
+const Separator = styled.hr`
+  border;0.5px solid lightgray;
+`;
