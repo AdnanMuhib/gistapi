3c97614531285ad6a2cf2bf14d21cd5e0f4e3e9f
Adnan Muhib <adnan.muhib@rocketmail.com>
2021-02-09 05:44:07 +0500

    Update Gist Component

--- src/components/Gist.js
+++ src/components/Gist.js
@@ -1,3 +1,32 @@
-const Gist = ({ gist }) => <>
+import React from "react";
 
-export default Gist
+import GistHeader from "./GistHeader";
+
+const Gist = ({ gist }) => {
+  const { owner: gist_owner } = gist;
+  const {
+    created_at,
+    updated_at,
+    files = {},
+    comments = 0,
+    forks_url,
+    comments_url,
+  } = gist;
+  return (
+    <div className="gist-wrapper">
+      <GistHeader
+        gist_owner={gist_owner}
+        gist_meta={{
+          created_at,
+          updated_at,
+          files,
+          comments,
+          comments_url,
+          forks_url,
+        }}
+      />
+    </div>
+  );
+};
+
+export default Gist;
